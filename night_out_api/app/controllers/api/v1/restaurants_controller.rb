55fc2c4c097d4e7c30878b5f23efc99129d63c71
timothyphilip <philippetimothe@gmail.com>
2020-12-05 17:08:08 -0500

    Restaurant controller show implementation.  Completed show view conditional accepting precise scope

--- night_out_api/app/controllers/api/v1/restaurants_controller.rb
+++ night_out_api/app/controllers/api/v1/restaurants_controller.rb
@@ -5,4 +5,20 @@ class Api::V1::RestaurantsController < ApplicationController
         render json: @restaurants, except: [:created_at, :updated_at]
     end
 
+    def show
+        if params[:id] && ((Api::V1::Restaurant.scope(params[:id]) == []) == false)
+            @restaurants = Api::V1::Restaurant.scope(params[:id])
+        else
+            @restaurants = Api::V1::Restaurant.all
+        end
+        render json: @restaurants, except: [:created_at, :updated_at]
+    end
+
 end
+
+    
+
+
+
+
+#  @restaurants = Api::V1::Restaurant.find_by_id(params[:id])
--- night_out_api/app/models/api/v1/restaurant.rb
+++ night_out_api/app/models/api/v1/restaurant.rb
@@ -1,3 +1,7 @@
 class Api::V1::Restaurant < ApplicationRecord
     belongs_to :location
+
+    def self.scope(location)
+        self.where(location_id:location)
+    end
 end
